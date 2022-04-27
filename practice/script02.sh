2ad31fc7488b5268c652b555a2f4459ed2c6aa45
John Parkhurst <jparkhurst120@gmail.com>
2022-04-27 00:29:08 -0400

    Create script02.sh

---
+++ practice/script02.sh
@@ -0,0 +1,15 @@
+#This is a script that will calculate the 
+#number of specified extension in specified directory
+#John Parkhurst
+
+echo "Begin:"
+echo "What is the extension you would like to search for? "
+read extType
+echo "What is the desired directory location? "
+read direcLoc
+
+#Now we search through the directory location
+
+ls direcLoc
+
+echo 5
